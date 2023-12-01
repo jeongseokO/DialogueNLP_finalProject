9a74a0a27c7b884c5b652c89feaa31fd7daf426f
jeongseoko <jeongseok0112@gmail.com>
2023-12-01 17:01:30 +0900

    add

--- run.sh
+++ run.sh
@@ -2,11 +2,14 @@
 
 #SBATCH --job-name=test
 #SBATCH --nodes=1
+#SBATCH --gres=gpu:1
 #SBATCH --time=0-24:00:00
 #SBATCH --mem=100000MB
 #SBATCH --cpus-per-task=8
 #SBATCH --output=slurm_output/%j.out
-source /home/${USER}/.bashrc
-source ~/anaconda3/etc/profile.d/conda.sh
 
-srun python test.py
+source /home/${USER}/.bashrc
+source ~/miniconda3/etc/profile.d/conda.sh
+conda activate env01
+
+srun python llama_test.py
